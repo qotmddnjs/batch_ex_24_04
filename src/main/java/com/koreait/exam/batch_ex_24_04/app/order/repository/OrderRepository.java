71210bb978bf9c2779c0819df7e8b16dbb93ba1f
qotmddnjs <dudcnsaus@gmail.com>
2024-04-23 16:16:27 +0900

    between 범위 정산

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/order/repository/OrderRepository.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/order/repository/OrderRepository.java
@@ -1,16 +1,8 @@
 package com.koreait.exam.batch_ex_24_04.app.order.repository;
 
 import com.koreait.exam.batch_ex_24_04.app.order.entity.Order;
-import com.koreait.exam.batch_ex_24_04.app.order.entity.OrderItem;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-  Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
-
-  Page<OrderItem> findAllByIdBetween(long fromId, long toId, Pageable pageable);
 }
